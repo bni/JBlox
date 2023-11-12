c70396fa1dbc820b06a257c997da5d0266ad1336
bni <bnilsson@me.com>
2023-11-12 17:56:54 +0100

    Upgrade to cheerpj 3.0rc1

--- compile.sh
+++ compile.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo making JBlox
-rm jblox.jar jblox.jar.js
+echo Compiling jblox.jar
 javac org/hn/jblox/*.java
 jar cf jblox.jar org/hn/jblox/*.class org/hn/jblox/img/*.gif
-/Applications/cheerpj/cheerpjfy.py jblox.jar
 #python3 -m http.server 8080
